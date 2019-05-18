36c81027c093c284efd6c004a549a3a4a6fd99db
Raissa Queiroz <raissa@h4money.com.br>
2019-05-18 00:52:35 -0300

    Vesão Inicial do Jogo do Dado

---
+++ jogo_do_dado/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <title> CURSO JS PRÁTICO - JOGO DO DADO </title>
+    </head>
+    <body align="center">
+        <h1> CURSO JS PRÁTICO - JOGO DO DADO </h1>
+        <img src="img/face1.png" alt="Face 1 do Dado" id="face"/>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
