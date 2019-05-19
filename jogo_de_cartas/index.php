416579fac3d8b5c80b3a74a5bb46bde6aa8b3827
Raissa Queiroz <raissa@h4money.com.br>
2019-05-19 18:44:24 -0300

    Projeto Jogo de Cartas

---
+++ jogo_de_cartas/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <title> CURSO JS PRÁTICO - JOGO DE CARTAS</title>
+    </head>
+    <body align="center">
+        <h1> CURSO JS PRÁTICO - JOGO DE CARTAS </h1>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
